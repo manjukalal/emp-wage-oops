35709cdfa80b687b5049d98f5627aab2a9d93f44
manjukalal <manjukalal1496@gmail.com>
2022-06-06 23:34:47 +0530

    Added:logic wage calculation for multiple companies

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,48 +1,108 @@
+
+
 public class EmployeeWage {
 
-            public static final int emprateperhrs = 20;
-            public static final int empfulltime = 1;
-            public static final int empparttime = 2;
-            public static final int empmaxhrs = 100;
-            public static final int empnoworkingdays = 20;
-            public static void employeeWage() {
+        public static void bridgeLabz(int emprateperhrs, int empnoworkingdays, int empmaxhrs) {
+            int emphr = 0;
+            int totalemphrs = 0;
+            int emptotalworkingdays = 0;
+            int empwage = 0;
+            while (totalemphrs <= empmaxhrs && emptotalworkingdays < empnoworkingdays) {
+                emptotalworkingdays++;
+                int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (empcheck) {
+                    case 1:
+                        emphr = 4;
+                        break;
 
-                int emphr = 0;
-                int totalemphrs = 0;
-                int emptotalworkingdays = 0;
-                int empwage = 0;
-                while (totalemphrs <= empmaxhrs && emptotalworkingdays < empnoworkingdays) {
-                    emptotalworkingdays++;
-                    int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+                    case 2:
+                        emphr = 8;
+                        break;
 
-                    switch (empcheck) {
-                        case 1:
-                            emphr = 4;
-                            break;
-
-                        case 2:
-                            emphr = 8;
-                            break;
-
-                        default:
-                            emphr = 0;
-                    }
-                    totalemphrs += emphr;
-
-                    System.out.println(emptotalworkingdays + "emphrs" + emphr);
+                    default:
+                        emphr = 0;
                 }
-                empwage = (emphr * emprateperhrs);
-                System.out.println("per day emp wage for " + emphr + "is:" + empwage);
-                int hrs = totalemphrs * emprateperhrs;
-                System.out.println("total wage for " + totalemphrs + "hrs is :" + hrs);
-            }
+                totalemphrs += emphr;
 
-            public static void main(String[] args) {
-                employeeWage();
             }
+            empwage = (emphr * emprateperhrs);
+            System.out.println("bridgelabz company wage:");
+            System.out.println("per day emp wage for " + emphr + "is:" + empwage);
+            int hrs = totalemphrs * emprateperhrs;
+            System.out.println("total wage for " + totalemphrs + "hrs is :" + hrs);
         }
 
 
 
+        static void gmr(int emprateperhrs, int empnoworkingdays, int empmaxhrs) {
+            int emphr = 0;
+            int totalemphrs = 0;
+            int emptotalworkingdays = 0;
+            int empwage = 0;
+            while (totalemphrs <= empmaxhrs && emptotalworkingdays < empnoworkingdays) {
+                emptotalworkingdays++;
+                int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (empcheck) {
+                    case 1:
+                        emphr = 4;
+                        break;
 
+                    case 2:
+                        emphr = 8;
+                        break;
+
+                    default:
+                        emphr = 0;
+                }
+                totalemphrs += emphr;
+
+            }
+            empwage = (emphr * emprateperhrs);
+            System.out.println("GMR company wage:");
+            System.out.println("per day emp wage for " + emphr + "is:" + empwage);
+            int hrs = totalemphrs * emprateperhrs;
+            System.out.println("total wage for " + totalemphrs + "hrs is :" + hrs);
+        }
+
+
+
+        static void Tesla(int emprateperhrs, int empnoworkingdays, int empmaxhrs) {
+            int emphr = 0;
+            int totalemphrs = 0;
+            int emptotalworkingdays = 0;
+            int empwage = 0;
+            while (totalemphrs <= empmaxhrs && emptotalworkingdays < empnoworkingdays) {
+                emptotalworkingdays++;
+                int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (empcheck) {
+                    case 1:
+                        emphr = 4;
+                        break;
+
+                    case 2:
+                        emphr = 8;
+                        break;
+
+                    default:
+                        emphr = 0;
+                }
+                totalemphrs += emphr;
+            }
+            empwage = (emphr * emprateperhrs);
+            System.out.println("Tesla company wage:");
+            System.out.println("per day emp wage for " + emphr + "is:" + empwage);
+            int hrs = totalemphrs * emprateperhrs;
+            System.out.println("total wage for " + totalemphrs + "hrs is :" + hrs);
+        }
+
+
+        public static void main(String[] args)
+        {
+            EmployeeWage wage = new EmployeeWage();
+            wage.bridgeLabz(20, 20, 100);
+            wage.gmr(30,25,120);
+            wage.Tesla(40,30,150);
+
+        }
+    }
 
